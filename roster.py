14beeb292b8d0fbe9a53a369441d87b3caa72710
peijiayi1010 <peijiayi@ad.unc.edu>
2025-11-10 23:17:28 -0500

    roster list:

---
+++ roster.py
@@ -0,0 +1,10 @@
+# https://goheels.com/sports/mens-basketball/roster
+import pandas as pd
+roster = ["Bacot", "Davis", "Cadeau"]
+data = pd.DataFrame(roster)
+print(data)
+
+
+roster = ["Bacot", "Davis", "Cadeau"]
+for player in roster:
+    print(player)
